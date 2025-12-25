123fbccbe5b9535b427cc6573649a19db3d5497c
henriquepablo <pablo269704@gmail.com>
2025-12-25 14:01:35 -0300

    feat: add component button

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -4,7 +4,9 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#D0D2D8',
+        paddingTop: 62,
+        padding: 24
     },
     logo: {
         height: 34,
